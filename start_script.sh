c421236e8229201a5bcb1f344894ff8f31440988
niconicoyang <a1-6@niconicoyangdeMacBook-Air.local>
2025-05-22 16:54:58 +0800

    补充修改文档

---
+++ start_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/cat.Xauthority
+
+echo "$(date) - Starting test.py" >>/home/cat/rknn_toolkit_lite2/examples/yolov5_plant/startup.log
+
+source ~/miniconda3/bin/activate
+conda activate yolov5
+cd ~/rknn_toolkit_lite2/examples/yolov5_plant/
+python test.py
